3f730ed7ffd8037dd077812f7ff46573e9cf23b9
aurelien01 <45340517+aurelien01@users.noreply.github.com>
2018-12-02 21:04:06 +0100

    Add files via upload
    ajout du fichier javascript

---
+++ main.js
@@ -0,0 +1,26 @@
+//confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+
+//let text=setTimeout
+
+/*function test() {
+    window.onload=function()
+    setTimeout((function(){alert('hello')}),2000);
+}; */
+
+
+
+/*confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+if (confirm("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+setTimeout(2000);*/
+
+/*var video = prompt('salut mec tu veut voir l video');
+ affirmative =oui;
+if(affirmative){
+};*/
+
+
+
+
+if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")){
+    document.location.href=("https://www.youtube.com/watch?v=dQw4w9WgXcQ")};
+    setTimeout(10000);
