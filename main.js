57e53a09fd49956ab46bdd264b4ea33481340422
aurelien01 <45340517+aurelien01@users.noreply.github.com>
2018-12-02 21:09:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,4 +23,4 @@ if(affirmative){
 
 if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")){
     document.location.href=("https://www.youtube.com/watch?v=dQw4w9WgXcQ")};
-    setTimeout(10000);
+    
